77b000e0a72a139809d4616d6816ef89fd129d9a
Eugene Hong <ehong270@gmail.com>
2023-04-22 16:21:18 -0400

    added overflow error

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,17 +1,20 @@
 from typing import List
 
 class ShoppingCart:
-    def __init__(self):
+    def __init__(self, max_size):
         self.items = []
+        self.max_size = max_size
 
     def add(self, item):
+        if self.size() == self.max_size:
+            raise OverflowError("cannot add more items")
         self.items.append(item)
 
     def size(self):
         return len(self.items)
 
     def get_items(self):
-        pass
+        return self.items
 
     def get_total_price(self, price_map):
         pass
