e666b80c0998445d1d9709874d51b9d6f8e978c6
Eugene Hong <ehong270@gmail.com>
2023-04-22 16:04:02 -0400

    base of pytest

---
+++ shopping_cart.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class ShoppingCart:
+    def __init__(self):
+        pass
+
+    def add(self, item):
+        pass
+
+    def size(self):
+        pass
+
+    def get_items(self):
+        pass
+
+    def get_total_price(self, price_map):
+        pass
